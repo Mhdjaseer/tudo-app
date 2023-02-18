5ba8c8d89aafe43f402bca97c50d74454493d773
jaseer j <98450786+Mhdjaseer@users.noreply.github.com>
2023-02-18 13:19:49 +0530

    project added

---
+++ Readme.md
@@ -0,0 +1,43 @@
+In this Python project we used fastAPI ,sqllite and Custom template to do this task.
+---
+---
+
+# Python version 
+    Python 3.11.2
+
+***
+
+## Steps to run the tudo project 
+
+    1) Create Virtual Environment:-
+*      First, create a directory name it todoapp and open terminal, and cd into todoapp then you need to type the following command.
+>       virtualenv env
+*      Now activate the virtual environment
+>       source env/bin/activate
+
+***
+
+
+
+
+    2) Installation:-
+*      FInstall FastAPI. make sure your environment is activated
+>       pip install fastapi
+*      Also, install uvicorn to work as the server
+>       pip install uvicorn
+*      Install package for templating
+>       pip install jinja2 python-multipart
+*      Install package for database
+>       pip install sqlalchemy
+
+***
+
+
+
+
+ ##   3) To run:-
+ *     To run the FastAPI app type the following command and hit enter
+>       uvicorn main:app --reload
+
+==========================================================================================
+# Open the link on the browser (http://127.0.0.1:8000)
---
+++ main.py
@@ -0,0 +1,66 @@
+from fastapi import FastAPI, Request, Depends, Form, status
+from fastapi.templating import Jinja2Templates
+import models
+from database import engine, sessionlocal
+from sqlalchemy.orm import Session
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+
+models.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates") #using templates for the view.
+
+#defining  fast API to the app variable
+app = FastAPI() 
+
+#static file directory 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+#db connection
+def get_db():
+    db = sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# first destination 
+@app.get("/")#That line shows the URL where your app is being served, in your local machine.
+async def home(request: Request, db: Session = Depends(get_db)):
+    #the values from the database
+    todos = db.query(models.Todo).order_by(models.Todo.id.desc()) 
+    return templates.TemplateResponse("index.html", {"request": request, "todos": todos})
+
+#post adding 
+@app.post("/add")
+async def add(request: Request, task: str = Form(...), db: Session = Depends(get_db)):
+    todo = models.Todo(task=task)
+    db.add(todo)
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
+
+#post editing 
+@app.get("/edit/{todo_id}")
+async def add(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    return templates.TemplateResponse("edit.html", {"request": request, "todo": todo})
+
+
+#if the post is edited 
+@app.post("/edit/{todo_id}")
+async def add(request: Request, todo_id: int, task: str = Form(...), completed: bool = Form(False), db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    todo.task = task
+    todo.completed = completed
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
+
+
+#deleting the post
+@app.get("/delete/{todo_id}")
+async def add(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    db.delete(todo)
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
